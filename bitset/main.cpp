f604e5c285220fa0eee9acebeabb20b3357a2e36
Ranoiaetep <ranoiaetep@me.com>
2021-06-22 20:16:56 -0700

    Removed unused cpp file

--- bitset/main.cpp
+++ bitset/main.cpp
@@ -2,7 +2,8 @@
 #include <type_traits>
 #include "bitset.hpp"
 
-int main(int argc, const char * argv[]) {
+int main(int argc, const char * argv[])
+{
     std::cout << bset::bitset<10>() + bset::bitset<10>();
     return 0;
 }
